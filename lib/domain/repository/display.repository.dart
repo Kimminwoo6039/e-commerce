a9473cc7800fbced7f7fa27427dd515116dc42ab
kimminwoo <qkqhemforhs1@gmail.com>
2023-11-10 10:57:06 +0900

    Flutter 초기 셋팅

--- lib/domain/repository/display.repository.dart
+++ lib/domain/repository/display.repository.dart
@@ -5,6 +5,7 @@ import '../../data/dto/common/response_wrapper/response_wrapper.dart';
 import '../../data/dto/display/menu/menu.dto.dart';
 import '../model/display/menu/menu.model.dart';
 
+
 abstract class DisplayRepository extends Repository {
   Future<ResponseWrapper<List<Menu>>> getMenusByMallType({
     required MallType mallType,
